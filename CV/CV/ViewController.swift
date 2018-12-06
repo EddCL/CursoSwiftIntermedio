c68289ea2be906fe5564074df174e91f231dc413
EddCL <38968021+EddCL@users.noreply.github.com>
2018-12-05 18:38:49 -0600

    Add files via upload

---
+++ CV/CV/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CV
+//
+//  Created by Macbook on 12/5/18.
+//  Copyright © 2018 iosLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
